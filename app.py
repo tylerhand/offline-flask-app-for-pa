3a556001af01ee11b44997ecbe5c7849624767be
Tyler Hand <tylerandrewhand@gmail.com>
2019-06-22 10:48:43 -0700

    Add HTTP 503 response in app.py to all requests

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 purpose='coming-soon'
 email='hand7029@vandals.uidaho.edu'
 
+@app.errorhandler(503)
+def page_not_found(e):
+    if purpose == 'maintenance':
+        title='Down for Maintenance'
+        return render_template('offline_page.html',email=email,title=title), 503
+    if purpose == 'coming-soon':
+        title='Coming Soon!'
+        return render_template('coming_soon.html',email=email,title=title), 503
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    if purpose == 'maintenance':
-        title='Down for Maintenance'
-        return render_template('offline_page.html',email=email,title=title)
-    if purpose == 'coming-soon':
-        title='Coming Soon!'
-        return render_template('coming_soon.html',email=email,title=title)
-
+    abort(503)
+    
 if __name__ == '__main__':
     app.run()
